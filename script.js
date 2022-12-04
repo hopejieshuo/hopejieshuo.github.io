f117df28d367f1a9e5806eda8e941da1e41a8783
hopejieshuo <101780409+hopejieshuo@users.noreply.github.com>
2022-12-04 09:46:34 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    var oMeta = document.createElement('meta');
-    oMeta.content = "width=device-width, mininum-scale=1.0, maxinum-scale=1.0, user-scalable=no";
-    oMeta.name = "viewport";
-    document.getElementsByTagName("head")[0].appendChild(oMeta);
-});
