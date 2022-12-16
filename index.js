df1284e4240eb4e307b031a272da00c0f45c0c69
hopejieshuo <101780409+hopejieshuo@users.noreply.github.com>
2022-12-16 14:22:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ window.onload=function(){
     if(location.href.indexOf("index.html") == -1){
         window.open("./index.html","_self");
     }
+    var options = {
+        strings:["hopejieshuo", 
+                "A GDI Malware Maker", 
+                "Like Math, English And Chemistry", 
+                "Currently Learning C/C++", 
+                "Welcome To Visit My Website"],
+        typeSpeed:100,
+        backSpeed:25,
+        loop:true,
+        cursorChar:"",
+        startDelay:500,
+        backDelay:500
+    };
+    var typed = new Typed('.title-main', options);
 }
